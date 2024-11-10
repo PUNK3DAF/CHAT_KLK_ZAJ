74f73f72b74d9795b9da75686c56bc60dcf31b10
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-11-10 16:43:08 +0100

    inicijalni komit

---
+++ src/model/Admin.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class Admin implements Serializable {
+
+    private int adminId;
+    private String email;
+    private String pass;
+    private String ime;
+    private String prezime;
+
+    public Admin(int adminId, String email, String pass, String ime, String prezime) {
+        this.adminId = adminId;
+        this.email = email;
+        this.pass = pass;
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Admin() {
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return ime + " " + prezime;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Admin other = (Admin) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return Objects.equals(this.pass, other.pass);
+    }
+
+}
